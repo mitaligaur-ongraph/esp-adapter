ae537318943d8cd065884c812694e756867443f9
Mitali <mitali.gaur@ongraph.com>
2024-03-22 14:07:32 +0530

    add base inherit

--- lib/esp_adapter_gem.rb
+++ lib/esp_adapter_gem.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "esp_adapter_gem/version"
+require_relative "esp_adapter_gem/base"
 require "esp_adapter_gem/mailchimp"
-require "esp_adapter_gem/base"
 
 module EspAdapterGem
   class Error < StandardError; end
