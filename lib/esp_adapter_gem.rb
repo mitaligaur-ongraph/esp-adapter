3f454860588c30bfb3f9733a180ea7ecd825726b
Mitali <mitali.gaur@ongraph.com>
2024-03-22 14:12:28 +0530

    check import

--- lib/esp_adapter_gem.rb
+++ lib/esp_adapter_gem.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "esp_adapter_gem/version"
-require_relative "esp_adapter_gem/base"
+# require_relative "esp_adapter_gem/base"
 require "esp_adapter_gem/mailchimp"
 
 module EspAdapterGem
   class Error < StandardError; end
+
   def self.description
     puts 'Test gem EspAdapter'
   end
